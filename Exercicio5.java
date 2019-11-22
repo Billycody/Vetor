0d45ef60fed8b92cf8a0398b8d187ddb61771a8e
Billycody <56124149+Billycody@users.noreply.github.com>
2019-11-22 20:08:27 -0300

    Update Exercicio5.java

--- Exercicio5.java
+++ Exercicio5.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-public class Exercicio5 {
+public class v5 {
 	public static void main(String[] args) {
-	Scanner in = new Scanner (System.in);
+	Scanner leia = new Scanner (System.in);
 	
 	int a[] = new int [10];
 	int i, n;
 	
 	for(i=0; i<10; i++) {
 		System.out.println("Informe o numero");
-		a[i] = in.nextInt();
+		a[i] = leia.nextInt();
 		
 		for(n=1; n<=a[i]; n++) {
 			if(a[i]%n == 0) {
